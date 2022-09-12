6d81d65fc020ce76f26273c9250dc4668029a064
Kristina Krajevska <krajevska.kristina98@gmail.com>
2022-09-12 10:51:17 +0200

    wip

--- src/places/pages/NewPlace.tsx
+++ src/places/pages/NewPlace.tsx
@@ -1,5 +1,115 @@
-import React from "react";
+import styled from "@emotion/styled";
+import React, { useCallback, useReducer } from "react";
+import { Button } from "shared/components/FormElements/Button";
+import { Input, InputTypes } from "shared/components/FormElements/Input";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "shared/util/validators";
 
-export const NewPlace = () => {
-  return <div>NewPlace</div>;
+interface Title {
+  value: string;
+  isValid: boolean;
+}
+
+interface Description extends Title {}
+
+interface Inputs {
+  title: Title;
+  description: Description;
+}
+interface State {
+  inputs: Inputs;
+  isValid: boolean;
+}
+
+interface Action {
+  type: "INPUT_CHANGE";
+  isValid: boolean;
+  inputId: InputTypes;
+  value: string;
+}
+
+const formReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid =
+            formIsValid && state.inputs[inputId as InputTypes].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
 };
+export const NewPlace = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback(
+    (id: InputTypes, value: string, isValid: boolean) => {
+      dispatch({
+        type: "INPUT_CHANGE",
+        value: value,
+        isValid: isValid,
+        inputId: id,
+      });
+    },
+    []
+  );
+  return (
+    <Form>
+      <Input
+        id="title"
+        elementType="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+      />
+      <Input
+        id="description"
+        elementType="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
+    </Form>
+  );
+};
+
+const Form = styled.form`
+  list-style: none;
+  margin: 0 auto;
+  padding: 1rem;
+  width: 90%;
+  max-width: 40rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 6px;
+  background: white;
+`;
---
+++ src/shared/components/FormElements/Input.tsx
@@ -0,0 +1,161 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import React, {
+  FC,
+  HTMLInputTypeAttribute,
+  useEffect,
+  useReducer,
+} from "react";
+import { validate, ValidatorT } from "shared/util/validators";
+export type InputTypes = "title" | "description";
+interface InputT {
+  elementType: string;
+  id?: InputTypes;
+  type?: HTMLInputTypeAttribute;
+  placeholder?: string;
+  label: string;
+  rows?: number;
+  errorText: string;
+  validators: ValidatorT[];
+  onInput: (id: InputTypes, value: string, isValid: boolean) => void;
+}
+
+interface State {
+  value: string;
+  isValid: boolean;
+}
+
+interface Action {
+  type: "CHANGE" | "TOUCH";
+  val?: string;
+  validators?: ValidatorT[];
+}
+
+const inputReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val || "",
+        isValid:
+          (action.validators && validate(action.val, action.validators)) ||
+          false,
+      };
+    case "TOUCH": {
+      return {
+        ...state,
+        isTouched: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export const Input: FC<InputT> = ({
+  elementType,
+  type,
+  id,
+  placeholder,
+  label,
+  rows,
+  errorText,
+  validators,
+  onInput,
+}) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+    isTouched: false,
+  });
+
+  useEffect(() => {
+    if (id) {
+      onInput(id, inputState.value, inputState.isValid);
+    }
+  }, [id, inputState.value, inputState.isValid, onInput]);
+
+  const changeHandler = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators,
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: "TOUCH",
+    });
+  };
+  const element =
+    elementType === "input" ? (
+      <InputField
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={inputState.value}
+        onChange={changeHandler}
+        invalid={!inputState.isValid}
+        onBlur={touchHandler}
+      />
+    ) : (
+      <TextArea
+        invalid={!inputState.isValid}
+        id={id}
+        rows={rows || 3}
+        value={inputState.value}
+        onBlur={touchHandler}
+        onChange={changeHandler}
+      />
+    );
+
+  return (
+    <FormControl>
+      <Label htmlFor={id}>{label}</Label>
+      {element}
+      {!inputState.isValid && <P invalid={!inputState.isValid}>{errorText}</P>}
+    </FormControl>
+  );
+};
+
+const FormControl = styled.div`
+  margin: 1rem 0;
+`;
+
+const Label = styled.label<{ invalid?: boolean }>`
+  display: block;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  color: ${(props) => props.invalid && "red"};
+`;
+
+const getSharedStyling = ({ invalid }: { invalid: boolean }) => css`
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  background: #f8f8f8;
+  padding: 0.15rem 0.25rem;
+  :focus {
+    outline: none;
+    background: #ebebeb;
+    border-color: #510077;
+  }
+  border-color: ${invalid && "red"};
+  background: ${invalid && "#ffd1d1"};
+`;
+
+const InputField = styled.input<{ invalid: boolean }>`
+  ${({ invalid }) => getSharedStyling({ invalid })}
+`;
+
+const TextArea = styled.textarea<{ invalid: boolean }>`
+  ${({ invalid }) => getSharedStyling({ invalid })}
+`;
+
+const P = styled.p<{ invalid?: boolean }>`
+  color: ${(props) => props.invalid && "red"};
+`;
